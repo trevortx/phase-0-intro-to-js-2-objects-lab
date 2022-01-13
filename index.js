eb0d66c61907b72d750b4efc78655be5f5e6553e
Trevor Vardeman <trevortx@gmail.com>
2022-01-12 21:56:20 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+  name: "Trevor",
+  streetAddress: "859 Vallejo St",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj,
+    [key]: value,
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key, value) {
+  const employee2 = {...obj};
+  delete employee2[key];
+  return employee2;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+ delete employee[key];
+ return employee;
+}
